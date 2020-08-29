48017c4b5a759fb31392818962ad490f86c4d788
Lucas Jerez <ltj2105@columbia.edu>
2020-08-29 19:36:11 +0200

    Updates

---
+++ precache-manifest.53abdd1b1cf03a379a4f680c1099be74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d89e456dc76d36fd16feeafdbf2ba9",
+    "url": "/heatmap_demo/index.html"
+  },
+  {
+    "revision": "a3ae7c7589f403d8bbf6",
+    "url": "/heatmap_demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d6fc6a9e0c7b317e4b8",
+    "url": "/heatmap_demo/static/js/2.fc96e2be.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/heatmap_demo/static/js/2.fc96e2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ae7c7589f403d8bbf6",
+    "url": "/heatmap_demo/static/js/main.07e318df.chunk.js"
+  },
+  {
+    "revision": "f3228c202982d5ffc4c1",
+    "url": "/heatmap_demo/static/js/runtime-main.227dcf12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/heatmap_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/heatmap_demo/precache-manifest.53abdd1b1cf03a379a4f680c1099be74.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/heatmap_demo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
